a550b2502b67449f88c0101ffbd302bc20791507
gmzln <gamze_ya@outlook.de>
2025-01-18 16:16:09 +0100

    building defeat monster logic

--- rpgGame.py
+++ rpgGame.py
@@ -2,11 +2,33 @@ inventory = []
 
 rooms = {
     'Hall' : {
-        'south' : 'Kitchen'
+        'south' : 'Kitchen',
+        'east' : 'Dining Room',
+        'item' : 'key',
     },
     'Kitchen' : {
-        'north' : 'Hall'
-    }
+        # 'north' : 'Hall', ungenutzte Code Zeile, da spieler vorher stirbt und somit keine Himmelsrichtung mehr angeben kann
+        'item' : 'monster',
+    },
+    'Dining Room' : { 'west' : 'Hall',
+                     'south' : 'Garden',
+                     'east' : 'Library',
+                     'item' : 'potion',
+                     },
+    'Garden' : { 'north' : 'Dining Room'},
+    'Library' : { 'east' : 'Office',
+                 'west' : 'Dining Rooon',
+                 'north' : 'Laboratory',
+                 'item' : 'BookOfLife'
+                 },
+    'Office' : { 'east' : 'Laboratory',
+                'west' : 'Library',
+                'item' : 'monster'
+                },
+    'Laboratory' : {'south': 'Library',
+                    'west' : 'Office',
+                    'item' : 'Beam-O-Mat'
+                    },
 }
 
 currentRoom = 'Hall'
@@ -14,21 +36,28 @@ currentRoom = 'Hall'
 def showInstructions():
     #print a main menu and the commands
     print('''
-    Welcome to your own RPG Game
+    Welcome to the RPG Game
     ----------------------------
 
-    Get to the Garden with a key and potion.
-    Avoid the monsters!
+    How to win:
+    Get to the Garden with a key and potion or
+    to the Laboratory with the BookOfLife 
+    and Beam-O-Mat.
+    
+    Avoid the monsters or defeat them!
 
     Commands:
         go [direction]
         get [item]
+        throw [potion]
+    ----------------------------
     ''')
 
 def showStatus():
     #print the players current status
+    print('Current status')
     print('--------------------------------')
-    print('You are in the' + currentRoom)
+    print('You are in the ' + currentRoom)
     #print the current inventory
     print('Inventory: ' + str(inventory))
     #print an item if there is one
@@ -47,7 +76,29 @@ while True:
     if move[0] == 'exit':
          break
     if move[0] == 'go':
-         if move[1] in rooms[currentRoom]:
-              currentRoom = rooms[currentRoom][move[1]]
-    else:
-        print('You cant go that way!')
+            if move[1] in rooms[currentRoom]:
+                currentRoom = rooms[currentRoom][move[1]]
+            else:
+                print('You can\'t go that way!')
+    if move[0] == 'get':
+            if 'item' in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+                inventory += [move[1]]
+                print(move[1] + ' got!')
+                del rooms[currentRoom]['item']
+            else:
+                print('Cant get ' + move[1] + '!')
+    if move[0] == 'throw':
+            if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item'] and 'potion' in inventory:
+                inventory.remove('potion')
+                print('You threw the potion! The monster is defeated!')
+                del rooms[currentRoom]['item']
+            else:
+                print('You can\'t throw the potion! Either there\'s no monster or you don\'t have a potion.')
+    if 'item' in rooms[currentRoom] and 'monster' and not 'potion' in rooms[currentRoom]['item']:
+         print('A monster has got you... GAME OVER!')
+         break
+    if (currentRoom == 'Garden' and 'key' in inventory and 'potion') or (currentRoom == 'Laboratory' and 'BookOfLife' in inventory and 'Beam-O-Mat') in inventory:
+         print('You escaped the house... YOU WIN!')
+         break
+    if move[0] == 'exit':
+         break
